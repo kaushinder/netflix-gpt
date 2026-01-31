c5534716056fa6a8142313ff1e1488ce45d64dae
kaushinder <kaushindersinghraghav@gmail.com>
2026-01-31 19:19:32 +0530

    Netflix-gpt project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-red-800">
+      Namaste, welcome to the React application called Netflix GPT!
+    </div>
+  );
+}
+
+export default App;
